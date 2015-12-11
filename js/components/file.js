69979a1a725dc579818c9e639ae5c16bc3f826a9
Kalin Chernev <kalin.chernev@gmail.com>
2015-12-11 11:30:09 +0100

    NEXTEUROPA-4905: file.js attached only when needed for the collapsible element in file translations
    Former-commit-id: d748e362d320717251505c147efda8131a5c67ab
    Former-commit-id: e91f1457be601f78f84b6f7887fdc7fef7b2fb17

---
+++ js/components/file.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * File component related behaviors.
+ */
+
+(function ($) {
+  Drupal.behaviors.dt_file = {
+    attach: function(context) {
+      "use strict";
+      console.log('loaded');
+    }
+  };
+})(jQuery);
