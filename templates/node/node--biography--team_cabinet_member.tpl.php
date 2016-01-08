439d7764d3f861e810dd2ffa601057e7ccebcb88
Kris Gunciarz <kris@waat.eu>
2016-01-08 16:18:15 +0000

    NEXTEUROPA-7712 adding css improvements for team cabinet members listing
    Former-commit-id: 910806191a31f643e9dac413e4d100507930304e
    Former-commit-id: e006ff730f2f4b28c032cfcd5746bb1effcdf267

--- templates/node/node--biography--team_cabinet_member.tpl.php
+++ templates/node/node--biography--team_cabinet_member.tpl.php
@@ -3,18 +3,15 @@
  * @file
  * Define the display of a custom view mode.
  */
-  $content['body']['#title'] = t('Responsibilities');
 ?>
 
 <div class="listing__top">
   <?php print render($content['field_biography_tagline']); ?>
   <?php print render($title_prefix); ?>
-  <h3 class="listing__title--grey-dark">
-    <?php print render($title); ?>
-  </h3>
+  <h3> <?php print render($title); ?> </h3>
   <?php print render($title_suffix); ?>
   <?php print render($content['field_biography_portrait']); ?>
-  <div class="listing__top__footer">
+  <div class="listing__top-footer">
     <?php print render($content['field_biography_email']); ?>
     <?php print render($content['field_biography_phone']) ?>
     <?php print render($content['field_social_networks']); ?>
