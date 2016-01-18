20c8e763c4011051af0c84b76aa19a760625f436
Harings Rob <haringsrob@gmail.com>
2016-01-18 12:58:00 +0100

    NEXTEUROPA-8271 putted wrapper inside of a href to align with ne-1col.tpl.php
    Former-commit-id: 4657074b16c256575c03023916a89da835a4fed0
    Former-commit-id: 0768449f8e9c82e1cad800040853ac9657bb6089

--- templates/ds/ne-2col-sidebar-no-link.tpl.php
+++ templates/ds/ne-2col-sidebar-no-link.tpl.php
@@ -12,38 +12,39 @@
     <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
   <?php if (!empty($second)): ?>
-  <<?php print $main_wrapper; ?> class="listing__column-main listing__column-main--sidebar-next column-main <?php print $main_classes; ?>">
     <?php if (!isset($prevent_link)): ?>
       <a href="<?php print $node_url; ?>" class="listing__item-link">
     <?php else: ?>
       <div class="listing__item-link">
     <?php endif; ?>
-      <?php print $main; ?>
+      <<?php print $main_wrapper; ?> class="listing__column-main listing__column-main--sidebar-next column-main <?php print $main_classes; ?>">
+        <?php print $main; ?>
+      </<?php print $main_wrapper; ?>>
     <?php if (!isset($prevent_link)): ?>
       </a>
     <?php else: ?>
       </div>
     <?php endif; ?>
-  </<?php print $main_wrapper; ?>>
-
   <<?php print $second_wrapper; ?> class="listing__column-second listing__column-second--no-link column-second <?php print $second_classes; ?>">
     <?php print $second; ?>
   </<?php print $second_wrapper; ?>>
 
   <?php else: ?>
-    <<?php print $main_wrapper; ?> class="listing__column-main column-main <?php print $main_classes; ?>">
-      <?php if (!isset($prevent_link)): ?>
-        <a href="<?php print $node_url; ?>" class="listing__item-link">
-      <?php else: ?>
-        <div class="listing__item-link">
-      <?php endif; ?>
+
+    <?php if (!isset($prevent_link)): ?>
+      <a href="<?php print $node_url; ?>" class="listing__item-link">
+    <?php else: ?>
+      <div class="listing__item-link">
+    <?php endif; ?>
+      <<?php print $main_wrapper; ?> class="listing__column-main column-main <?php print $main_classes; ?>">
         <?php print $main; ?>
-      <?php if (!isset($prevent_link)): ?>
-        </a>
-      <?php else: ?>
-        </div>
-      <?php endif; ?>
-    </<?php print $main_wrapper; ?>>
+      </<?php print $main_wrapper; ?>>
+    <?php if (!isset($prevent_link)): ?>
+      </a>
+    <?php else: ?>
+      </div>
+    <?php endif; ?>
+
   <?php endif; ?>
 </<?php print $layout_wrapper ?>>
 
