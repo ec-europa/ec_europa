02ac0cef8ecffc517967dcc3332abbf009883c2b
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-01 17:26:46 +0100

    NEXTEUROPA-8490: Split js for tabs to be conditionally excluded by php for home page of CWT
    Former-commit-id: 21d472d820810882ad26e557d4d4692366cce106
    Former-commit-id: 808fadd30ccedb1db6530bd614a9b92267e7f729

--- js/europa.js
+++ js/europa.js
@@ -13,31 +13,6 @@
   Drupal.europa.breakpoints.medium = 'screen and (min-width: 480px)';
   Drupal.europa.breakpoints.small = 'screen and (min-width: 768px)';
 
-  Drupal.behaviors.europa_tabs = {
-    attach: function (context) {
-      $('.nav-tabs--with-content').once('nav-tabs', function() {
-        $this = $(this);
-        if (typeof enquire !== 'undefined') {
-          // Runs on device width change.
-          enquire.register('screen and (max-width: 479px)', {
-            // Setup.
-            setup : function() {
-              $this.siblings('.tab-content').children().addClass('tab-pane');
-            },
-            // Mobile.
-            match : function() {
-              $this.siblings('.tab-content').children().removeClass('tab-pane');
-            },
-            // Desktop.
-            unmatch: function() {
-              $this.siblings('.tab-content').children().addClass('tab-pane');
-            }
-          });
-        }
-      });
-    }
-  };
-
   Drupal.behaviors.timeline = {
     attach: function(context) {
       var $timelineSelector = $('.timeline');
