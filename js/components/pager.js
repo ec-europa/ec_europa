3f6f8a8562e31d5dc5a7568ba91c20a911d484e1
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-03 17:03:57 +0100

    Start rewrite js to include back the listeners
    Former-commit-id: 64ba85cc62303c73cc93412cdda8d4af102fed79
    Former-commit-id: 8bdd5d08c4aa2fcfe966a24fabc3d1825aabd459

--- js/components/pager.js
+++ js/components/pager.js
@@ -6,52 +6,67 @@
 (function ($) {
   Drupal.behaviors.europa_pager = {
     attach: function(context) {
-      var options = '';
       $('ul.pager').once('pager', function(){
-        $('li.select', this).once('pagerselect', function(){
-          var $link = $('a', this);
+        $('li.select', this).once('pager__item', function(){
+          var listItem = this,
+              $link = $('a', listItem),
+              options = null,
+              selected = '',
+              value = '',
+              title = '';
+
           if ($link.length > 0) {
-            var value = $link.attr('href');
-            var title = Drupal.t('Page') + ' ' + $link.html();
-            var selected = '';
+            title = Drupal.t('Page') + ' ' + $link.html();
+            value = $link.attr('href');
           }
           else {
-            var value = '';
-            var title = Drupal.t('Page') + ' ' + $(this).html();
-            var selected = 'selected="selected"';
+            title = Drupal.t('Page') + ' ' + $(listItem).html();
+            selected = 'selected="selected"';
           }
-          options += '<option value="' + value + '"' + selected + '>' + title + '</option>';
-          $(this).hide();
+
+          if (options != '') {
+            options += '<option value="' + value + '"' + selected + '>' + title + '</option>';
+            $(listItem).hide();
+          }
+
         });
+
         if (options != '') {
-          var select = $('<select class="pager__dropdown">' + options + '</select>');
-          select.children().data('activation', 'activated').on({
-            keydown: function(event) {
-              if (event.which === 13) {
-                if ($(this).data('activation') === 'paused') {
-                  $(this).data('activation', 'activated');
-                  $(this).trigger('change');
-                }
-              }
-              else {
-                $(this).data('activation', 'paused');
-              }
-            },
-            click: function(event) {
-              if ($(this).data('activation') === 'paused') {
-                $(this).data('activation', 'activated');
-                $(this).trigger('change');
-              }
-            },
-            change: function(event) {
-              if ($(this).data('activation') === 'activated') {
-                var optionHref = $(this).val(),
-                  $pagerItem = $('.pager__item:hidden');
-                $pagerItem.children('a[href="' + optionHref + '"]').click();
-              }
-            }
+          $(options).each(function(i) {
+            var option = this;
+            $(option).data('activation', 'activated');
           });
-          $('.pager__combo-container', this).before(select);
+
+          var $select = $('<select class="pager__dropdown">' + options + '</select>');
+
+
+          //    .data('activation', 'activated').on({
+          //  keydown: function(event) {
+          //    if (event.which === 13) {
+          //      if ($(this).data('activation') === 'paused') {
+          //        $(this).data('activation', 'activated');
+          //        $(this).trigger('change');
+          //      }
+          //    }
+          //    else {
+          //      $(this).data('activation', 'paused');
+          //    }
+          //  },
+          //  click: function(event) {
+          //    if ($(this).data('activation') === 'paused') {
+          //      $(this).data('activation', 'activated');
+          //      $(this).trigger('change');
+          //    }
+          //  },
+          //  change: function(event) {
+          //    if ($(this).data('activation') === 'activated') {
+          //      var optionHref = $(this).val(),
+          //        $pagerItem = $('.pager__item:hidden');
+          //      $pagerItem.children('a[href="' + optionHref + '"]').click();
+          //    }
+          //  }
+          //});
+          $('.pager__combo-container', this).before($select);
           $('.pager__combo-container').hide();
         }
       });
