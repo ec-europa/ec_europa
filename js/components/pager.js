68b82b3aa5627395efb9365876343cb0ca972d18
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-04 10:18:56 +0100

    Trying to put back the js
    Former-commit-id: 910883981c42ec718a7113da874df1c8b0e3991a
    Former-commit-id: 0d9e0c66f1b0680a8b5639da25530434c4fa8d9b

--- js/components/pager.js
+++ js/components/pager.js
@@ -6,14 +6,15 @@
 (function ($) {
   Drupal.behaviors.europa_pager = {
     attach: function(context) {
-      $('ul.pager').once('pager', function(){
-        var options = null;
-        $('li.select', this).once('pager__item', function(){
+      $('ul.pager').once('pager', function() {
+        var options = '',
+            pagerContainer = '.pager__combo-container';
+        $('li.select', this).once('pager__item', function() {
           var listItem = this,
               $link = $('a', listItem),
               selected = '',
-              value = '',
-              title = '';
+              title = '',
+              value = '';
 
           if ($link.length > 0) {
             title = Drupal.t('Page') + ' ' + $link.html();
@@ -26,16 +27,37 @@
           options += '<option value="' + value + '"' + selected + '>' + title + '</option>';
           $(listItem).hide();
         });
-        if (options != ''){
+        // if the .once on .pager__items has aggregated markup for the options, make the select.
+        if (options != '') {
           var $select = $('<select class="pager__dropdown">' + options + '</select>');
-          $select.on({
-            change: function(){
-              var target = String($(this).val());
-              window.location.href = $("a[href='" + target + "']").first().attr('href');
+          $select.children().data('activation', 'activated').on({
+            keydown: function(event) {
+              if (event.which === 13) {
+                if ($(this).data('activation') === 'paused') {
+                  $(this).data('activation', 'activated');
+                  $(this).trigger('change');
+                }
+              }
+              else {
+                $(this).data('activation', 'paused');
+              }
+            },
+            click: function(event) {
+              if ($(this).data('activation') === 'paused') {
+                $(this).data('activation', 'activated');
+                $(this).trigger('change');
+              }
+            },
+            change: function(event) {
+              if ($(this).data('activation') === 'activated') {
+                var optionHref = $(this).val(),
+                    $pagerItem = $('.pager__item:hidden');
+                $pagerItem.children('a[href="' + optionHref + '"]').click();
+              }
             }
           });
-          $('.pager__combo-container', this).before($select);
-          $('.pager__combo-container').hide();
+          $(pagerContainer, this).before($select);
+          $(pagerContainer).hide();
         }
       });
     }
