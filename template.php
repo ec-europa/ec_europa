bd923e709070ee0f80708a5d387058bfb1c0904a
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-05 11:14:47 +0100

    NEXTEUROPA-8622: Updates
    Former-commit-id: 335f838954a58dd8a3afed3da071b92364fc9a31
    Former-commit-id: 337c5781ea68db517dce910a278c425b5e448081

--- template.php
+++ template.php
@@ -1281,7 +1281,7 @@ function europa_pager($variables) {
     }
     $items[] = array(
       'class' => array('pager__item pager__middle'),
-      'data' => "<span class='pager__combo-container'><span class='pager__combo-current'>" . t('Page !page', array('!page' => $pager_current)) . '&nbsp;</span>' .
+      'data' => "<span class='pager__separator'></span><span class='pager__combo-container'><span class='pager__combo-current'>" . t('Page !page', array('!page' => $pager_current)) . '&nbsp;</span>' .
       '<span class="pager__combo-total">' . t('of !total', array('!total' => $pager_max)) . '</span>' .
       '</span>',
     );
