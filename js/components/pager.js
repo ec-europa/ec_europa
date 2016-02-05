c15020e745ed77bd590743cadad46c5a7c718594
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-05 17:37:22 +0100

    NEXTEUROPA-8622: Updates
    Former-commit-id: 0a64991632f966b5483686ced8ce498b8130a205
    Former-commit-id: 02725364fbcd260cd2475888a5bf7665f7b9e60f

--- js/components/pager.js
+++ js/components/pager.js
@@ -13,19 +13,21 @@
         $('li.select', this).once('pager__item', function() {
           var listItem = this,
               $link = $('a', listItem),
+              title = Drupal.t('Page'),
+              pageNum = '',
               selected = '',
-              title = '',
               value = '';
 
           if ($link.length > 0) {
-            title = Drupal.t('Page') + ' ' + $link.html();
+            pageNum = $link.text();
             value = $link.attr('href');
           }
           else {
-            title = Drupal.t('Page') + ' ' + $(listItem).html();
-            selected = 'selected="selected"';
+            pageNum = $(listItem).text();
+            selected = 'selected';
           }
-          options += '<option value="' + value + '"' + selected + '>' + title + '</option>';
+          option = ('<option value="' + value + '" ' + selected + '>' + title + ' ' + pageNum  + '</option>').trim();
+          options += option;
           $(listItem).hide();
         });
         // If the .once on .pager__items has aggregated markup for the options, make the select.
