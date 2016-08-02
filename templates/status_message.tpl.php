8562df15c3bcc71b1345063bee8cfdd15926f189
Janos Kuszing <github.com@office.netstudio.hu>
2016-08-02 11:39:48 +0200

    DTTSB-654 (Events: practical information block) (#508)
    * DTTSB-654: Reverting force and reexporting dt_event and dt_event_default feature again.
    
    * DTTSB-654: Exported ds filed.
    
    * DTTSB-654: Added type of event.
    
    * DTTSB-654: Adding formatting to weblink.
    
    * DTTSB-654: No reason to keep a features.field_base.inc at default settings.
    
    * DTTSB-654: Started to refactor the status message, not finished yet.
    
    * DTTSB-654: Started using new formatters.
    
    * DTTSB-654: New template for status message.
    
    * DTTSB-654: Added status label.
    
    * DTTSB-654: Refactoring displaying dates.
    
    * DTTSB-654: Displaying status message.
    
    * DTTSB-654: Should not add registration link to see attand.
    
    * DTTSB-654: Should not add registration link to see attand.
    
    * DTTSB-654: Event behat test, not finished.
    
    * DTTSB-654: Updating tests.
    
    * DTTSB-654: events test.
    
    * DTTSB-654: Test for link no title.
    
    * DTTSB-654: Final fixes.
    
    * DTTSB-654: Formatting code.
    
    * DTTSB-654: Feature update on dt_event, dt_event_default.
    
    * DTTSB-654: Remove CRLF from commissioner.info.
    
    * DTTSB-654: Remove CRLF from commissioner.info.
    
    * DTTSB-654: Update behat tests.
    
    * DTTSB-654: Removing status message on past event.
    
    * DTTSB-654: added final point to reschedule message

--- templates/status_message.tpl.php
+++ templates/status_message.tpl.php
@@ -5,7 +5,7 @@
  * Contains the markup for the message block.
  */
 ?>
-<div class="messages<?php print $message_classes; ?>">
+<div class="messages <?php print $message_classes; ?>">
 
   <?php if ($message_title): ?>
     <h3><?php print ($message_type ? '<span class="sr-only">' . $message_type . ': </span>' : '') . $message_title; ?></h3>
