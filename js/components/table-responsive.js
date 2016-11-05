22a09efe0ea366e4097b6a1ce4fd6d7a8d839403
Nelson Matias <grin.correio@gmail.com>
2016-11-05 01:31:46 +0100

    Other improvements.

--- js/components/table-responsive.js
+++ js/components/table-responsive.js
@@ -7,17 +7,17 @@
   Drupal.behaviors.europa_table = {
     attach: function (context) {
       $('.table-responsive').each(function () {
-        var headerText = [];
-                // The rows in a table body.
-                tableRows = $('tbody tr', $(this));
-                // The headers in a table.
-                headers = $('thead tr th', $(this));
-                // The number of main headers.
-                headFirst = $('thead tr:first th', $(this)).length - 1;
-                // Number of cells per row.
-                cellPerRow = $('tbody tr:first td', $(this)).length;
-                // Position of the eventual colspan element.
-                colspanIndex = $('th[colspan]', $(this)).index();
+        var headerText = [],
+          // The rows in a table body.
+          tableRows = $('tbody tr', $(this)),
+          // The headers in a table.
+          headers = $('thead tr th', $(this)),
+          // The number of main headers.
+          headFirst = $('thead tr:first th', $(this)).length - 1,
+          // Number of cells per row.
+          cellPerRow = $('tbody tr:first td', $(this)).length,
+          // Position of the eventual colspan element.
+          colspanIndex = $('th[colspan]', $(this)).index();
 
         // Build the array with all the "labels".
         for (var i = 0; i < headers.length; i++) {
@@ -27,9 +27,9 @@
 
         // If we have a colspan, we have to prepare the data for it.
         if (colspanIndex !== -1) {
-          var textColspan = headerText.splice(colspanIndex, 1);
-          var ci = colspanIndex;
-          var cn = $('th[colspan]', $(this)).attr('colspan');
+          var textColspan = headerText.splice(colspanIndex, 1),
+            ci = colspanIndex,
+            cn = $('th[colspan]', $(this)).attr('colspan');
 
           for (var c = 0; c < cn; c++) {
             headerText.splice(ci + c, 0, headerText[headFirst + c]);
@@ -41,12 +41,12 @@
         tableRows.each(function () {
           for (var j = 0; j < cellPerRow; j++) {
             if (headerText[j] == '' || headerText[j] == '\u00a0') {
-                $('td', $(this)).eq(j).attr({
-                  "class": "table-responsive__heading"
-                });
+              $('td', $(this)).eq(j).attr({
+                "class": "table-responsive__heading"
+              });
             }
             else {
-                  $('td', $(this)).eq(j).attr("data-th", headerText[j]);
+              $('td', $(this)).eq(j).attr("data-th", headerText[j]);
             }
 
             if (colspanIndex !== -1) {
@@ -56,9 +56,9 @@
               });
 
               for (var c = 1; c < cn; c++) {
-                  $('td', $(this)).eq(colspanIndex + c).attr({
-                    "class": "table-responsive__group_element"
-                  });
+                $('td', $(this)).eq(colspanIndex + c).attr({
+                  "class": "table-responsive__group_element"
+                });
               }
             }
           }
