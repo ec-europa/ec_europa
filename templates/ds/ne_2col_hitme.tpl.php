2f0c771f4105dd6922ec2e780258ea556ca020ac
Harings Rob <haringsrob@gmail.com>
2016-12-05 14:02:42 +0100

    DTTSB-1244 (Press contact listing) (#966)
    * DTTSB-1244 - Created feature with search api index and views
    
    * DTTSB-1244 - Adding context
    
    * DTTSB-1244 - phpcbf
    
    * DTTSB-1244 - Adding logic for the hitme library inclusiin
    
    * DTTSB-1244 - Updating person feature, updating views, facets not working
    
    * DTTSB-1244 - Updating person feature, updating views, facets not working
    
    * DTTSB-1244 - Aligning image to the right in listing two columns
    
    * DTTSB-1244 - Adding default facets export
    
    * DTTSB-1244 - Updating again person feature, press contacts
    
    * Remove dt_press_contacts and add test
    
    * Views update and test coverage
    
    * Code improvements, context export
    
    * String fixes
    
    * Update ne_2col_hitme.tpl.php
    
    * Test update test
    
    * Update file.feature

---
+++ templates/ds/ne_2col_hitme.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Display Suite NE Bootstrap Two Columns Hit Me.
+ */
+
+  // Add sidebar classes so that we can apply the correct width in css.
+  // Second block is needed to activate display suite support on forms.
+?>
+
+<<?php print $layout_wrapper . $layout_attributes; ?> class="<?php print $classes; ?>">
+  <?php if (isset($title_suffix['contextual_links'])) : ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <?php if (!isset($prevent_link)) : ?>
+    <div class="listing__item-link">
+  <?php else: ?>
+    <div class="listing__item-nolink">
+  <?php endif; ?>
+    <?php if (!empty($second)) : ?>
+      <<?php print $second_wrapper; ?> class="listing__column-second column-second <?php print $second_classes; ?>">
+        <?php print $second; ?>
+      </<?php print $second_wrapper; ?>>
+    <?php endif; ?>
+    <<?php print $main_wrapper; ?> class="listing__column-main column-main <?php print $main_classes; ?>">
+      <?php print $main; ?>
+    </<?php print $main_wrapper; ?>>
+    </div>
+</<?php print $layout_wrapper; ?>>
+
+<?php if (!empty($drupal_render_children)) : ?>
+  <?php print $drupal_render_children; ?>
+<?php endif; ?>
