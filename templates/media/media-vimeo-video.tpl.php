85c77017b6d6aaecb374e1aef21d908d8a5e2d26
Nelson Matias <grin.correio@gmail.com>
2017-01-25 15:25:04 +0100

    DTTSB-1524 (Event: issues on translations, active event status) (#1096)
    * Modified behat texts to translate fields and phrases.
    
    * Added HTML for the iframes to be reponsives.
    
    * Adding html to the prefix of the field scribble.
    
    * Behat test fixed.
    
    * Behat test fixed.
    
    * Removed invalid change in FeatureContext.php file.
    
    * Changes applied after peer review.

--- templates/media/media-vimeo-video.tpl.php
+++ templates/media/media-vimeo-video.tpl.php
@@ -19,5 +19,5 @@
  */
 ?>
 <div class="file__container <?php print $classes; ?> media-vimeo-<?php print $id; ?>">
-    <iframe class="media-vimeo-player" <?php print $api_id_attribute; ?>width="<?php print $width; ?>" height="<?php print $height; ?>" title="<?php print $title; ?>" src="<?php print $ec_embedded_video_url; ?>" frameborder="0" allowfullscreen><?php print $alternative_content; ?></iframe>
+  <iframe class="media-vimeo-player" <?php print $api_id_attribute; ?>width="<?php print $width; ?>" height="<?php print $height; ?>" title="<?php print $title; ?>" src="<?php print $ec_embedded_video_url; ?>" frameborder="0" allowfullscreen="allowfullscreen"><?php print $alternative_content; ?></iframe>
 </div>
