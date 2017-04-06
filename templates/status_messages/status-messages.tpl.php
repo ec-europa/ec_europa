4c8af85a844b3950dc1e4e932946be83c68ff427
Pol Dellaiera <d.paolino@gmail.com>
2017-04-06 23:16:34 +0200

    EUTHEME-174: Status messages component. (#44)

--- templates/status_messages/status-messages.tpl.php
+++ templates/status_messages/status-messages.tpl.php
@@ -6,14 +6,6 @@
  */
 ?>
 <?php foreach ($status_messages['status_messages'] as $type => $data): ?>
-    <div class="messages alert <?php print $type; ?>">
-        <a href="#" class="close" data-dismiss="alert">&times;</a>
-        <h3>
-            <span class="sr-only"><?php print render($data['#title']); ?></span>
-        </h3>
-        <p>
-          <?php print render($data); ?>
-        </p>
-    </div>
+  <?php print render($data); ?>
 <?php endforeach; ?>
 
