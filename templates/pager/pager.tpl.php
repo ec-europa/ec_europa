937bec75186d6b6598173caa363de32823a12713
Delphine <fefaine@gmail.com>
2017-04-11 10:37:10 +0200

    EUTHEME-183: Update Pager component. (#55)

---
+++ templates/pager/pager.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<div class="pager__wrapper">
+  <h2 class="sr-only"><?php print t('Pages'); ?></h2>
+  <?php print render($pager); ?>
+</div>
