3bc53a2eb2775a716b6ff9a9b81c0c13eb220c87
Delphine Lepers <fefaine@gmail.com>
2017-04-28 11:38:06 +0200

    EUTHEME-225: Update the Grid component implementation.

---
+++ templates/views/views-view-grid.tpl.php
@@ -0,0 +1,14 @@
+
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php require(drupal_get_path('theme', 'europa') .'/templates/grid/grid.tpl.php'); ?>
