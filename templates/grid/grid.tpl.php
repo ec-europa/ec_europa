179c6a600b0b8ce28585da5469dc839ca4509644
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-05-11 16:37:12 +0200

    NEPT-970: Update grid component.

--- templates/grid/grid.tpl.php
+++ templates/grid/grid.tpl.php
@@ -6,16 +6,20 @@
  */
 ?>
 <?php if (!empty($title)) : ?>
-  <h3><?php print $title; ?></h3>
+    <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="ecl-container">
-  <?php foreach ($rows as $row_number => $columns): ?>
-    <div class="ecl-row">
-    <?php foreach ($columns as $column_number => $item): ?>
-      <div class="ecl-col">
-        <?php print render($item); ?>
-      </div>
+<?php if (!empty($caption)) : ?>
+    <caption><?php print $caption; ?></caption>
+<?php endif; ?>
+
+<div class="ecl-container <?php print $class; ?>"<?php print $attributes; ?>>
+    <?php foreach ($rows as $row_number => $columns): ?>
+        <div class="ecl-row <?php print $row_classes[$row_number];?> ">
+          <?php foreach ($columns as $column_number => $item): ?>
+              <div class="ecl-col <?php print $column_classes[$row_number];?> ">
+                <?php print $item; ?>
+              </div>
+          <?php endforeach; ?>
+        </div>
     <?php endforeach; ?>
-    </div>
-  <?php endforeach; ?>
-</div>
+ </div>
