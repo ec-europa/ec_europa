f1a4a87ea759d33b9c96c222f1b174f59dc91089
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-05-16 18:22:49 +0200

    EUTHEME-262: IMplement ne 1 col templates inside the theme.

---
+++ ds_layouts/ne_1col/ne-1col.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Display Suite NE Bootstrap One Column.
+ */
+?>
+  <<?php print $layout_wrapper . $layout_attributes; ?> class="<?php print $classes; ?>">
+
+<?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+<?php endif; ?>
+
+<?php if (empty($hidelink) && !empty($node_url)) : ?>
+  <a href="<?php print $node_url; ?>" class="listing__item-link" title="<?php print $title; ?>"><span class="sr-only"><?php print $title; ?></span></a>
+<?php endif; ?>
+
+  <<?php print $main_wrapper; ?> class="listing__column-main <?php print $main_classes; ?>">
+<?php print $main; ?>
+  </<?php print $main_wrapper; ?>>
+
+  </<?php print $layout_wrapper; ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children; ?>
+<?php endif; ?>
