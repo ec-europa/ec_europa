4135f00aff65c01efa91b1c902f4b84a1bf2e221
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-06-23 16:41:49 +0200

    NEPT-1068: Update the template, add the missing condition.

--- templates/footer/footer.tpl.php
+++ templates/footer/footer.tpl.php
@@ -7,41 +7,43 @@
 ?>
 
 <footer class="ecl-footer">
-  <div class="ecl-footers__site-corporate">
-    <div class="ecl-container">
-      <?php if (!empty($footer_improved)): ?>
-      <div class="ecl-row">
-        <?php print render($footer_improved); ?>
-      </div>
-      <?php endif; ?>
-      <div class="ecl-row">
-      <?php if (!empty($footer_left)): ?>
-        <div class="ecl-col-sm ecl-footers__column">
-          <?php print render($footer_left); ?>
-        </div>
-      <?php endif; ?>
+    <div class="ecl-footers__site-corporate">
+        <div class="ecl-container">
+          <?php if (!empty($footer_improved)): ?>
+              <div class="ecl-row">
+                <?php print render($footer_improved); ?>
+              </div>
+          <?php endif; ?>
+            <div class="ecl-row">
+              <?php if (!empty($footer_left)): ?>
+                  <div class="ecl-col-sm ecl-footers__column">
+                    <?php print render($footer_left); ?>
+                  </div>
+              <?php endif; ?>
 
-      <?php if (!empty($footer_middle)): ?>
-        <div class="ecl-col-sm ecl-footers__column">
-          <?php print render($footer_middle); ?>
-        </div>
-      <?php endif; ?>
+              <?php if (!empty($footer_middle)): ?>
+                  <div class="ecl-col-sm ecl-footers__column">
+                    <?php print render($footer_middle); ?>
+                  </div>
+              <?php endif; ?>
 
-      <?php if (!empty($footer_right)): ?>
-        <div class="ecl-col-sm ecl-footers__column">
-          <?php print render($footer_right); ?>
+              <?php if (!empty($footer_right)): ?>
+                  <div class="ecl-col-sm ecl-footers__column">
+                    <?php print render($footer_right); ?>
+                  </div>
+              <?php endif; ?>
+            </div>
         </div>
-      <?php endif; ?>
-      </div>
     </div>
-  </div>
-  <div class="ecl-footers__ec">
-    <div class="ecl-container">
-      <div class="ecl-row">
-        <div class="ecl-col-sm">
-          <?php print render($footer_bottom); ?>
-        </div>
+  <?php if (!empty($footer_bottom)): ?>
+      <div class="ecl-footers__ec">
+          <div class="ecl-container">
+              <div class="ecl-row">
+                  <div class="ecl-col-sm">
+                    <?php print render($footer_bottom); ?>
+                  </div>
+              </div>
+          </div>
       </div>
-    </div>
-  </div>
+  <?php endif; ?>
 </footer>
