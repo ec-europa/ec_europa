0d5868960c415ac3f7c2fb6bfec2cdad0d412dd5
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-06-23 16:30:52 +0200

    NEPT-1068: Upgrading the footer component.

--- templates/footer/footer.tpl.php
+++ templates/footer/footer.tpl.php
@@ -9,16 +9,29 @@
 <footer class="ecl-footer">
   <div class="ecl-footers__site-corporate">
     <div class="ecl-container">
+      <?php if (!empty($footer_improved)): ?>
       <div class="ecl-row">
+        <?php print render($footer_improved); ?>
+      </div>
+      <?php endif; ?>
+      <div class="ecl-row">
+      <?php if (!empty($footer_left)): ?>
         <div class="ecl-col-sm ecl-footers__column">
           <?php print render($footer_left); ?>
         </div>
+      <?php endif; ?>
+
+      <?php if (!empty($footer_middle)): ?>
         <div class="ecl-col-sm ecl-footers__column">
           <?php print render($footer_middle); ?>
         </div>
+      <?php endif; ?>
+
+      <?php if (!empty($footer_right)): ?>
         <div class="ecl-col-sm ecl-footers__column">
           <?php print render($footer_right); ?>
         </div>
+      <?php endif; ?>
       </div>
     </div>
   </div>
