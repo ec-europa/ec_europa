3d080cb5cc70196a12442781754db52b1bd202d0
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-06-27 14:32:43 +0200

    NEPT-1068: Move the variables to  the page preprocess. Fix tpl.

--- templates/footer/footer.tpl.php
+++ templates/footer/footer.tpl.php
@@ -7,43 +7,38 @@
 ?>
 
 <footer class="ecl-footer">
-    <div class="ecl-footers__site-corporate">
-        <div class="ecl-container">
-          <?php if (!empty($footer_improved)): ?>
-              <div class="ecl-row">
-                <?php print render($footer_improved); ?>
-              </div>
-          <?php endif; ?>
-            <div class="ecl-row">
-              <?php if (!empty($footer_left)): ?>
-                  <div class="ecl-col-sm ecl-footers__column">
-                    <?php print render($footer_left); ?>
-                  </div>
-              <?php endif; ?>
-
-              <?php if (!empty($footer_middle)): ?>
-                  <div class="ecl-col-sm ecl-footers__column">
-                    <?php print render($footer_middle); ?>
-                  </div>
-              <?php endif; ?>
-
-              <?php if (!empty($footer_right)): ?>
-                  <div class="ecl-col-sm ecl-footers__column">
-                    <?php print render($footer_right); ?>
-                  </div>
-              <?php endif; ?>
-            </div>
+  <?php if (!empty($footer_improved)): ?>
+    <div class="ecl-footers__site-identity">
+      <div class="ecl-container">
+        <div class="ecl-row">
+          <?php print render($footer_improved); ?>
         </div>
-    </div>
-  <?php if (!empty($footer_bottom)): ?>
-      <div class="ecl-footers__ec">
-          <div class="ecl-container">
-              <div class="ecl-row">
-                  <div class="ecl-col-sm">
-                    <?php print render($footer_bottom); ?>
-                  </div>
-              </div>
-          </div>
       </div>
+    </div>
   <?php endif; ?>
+  <div class="ecl-footers__site-corporate">
+    <div class="ecl-container">
+      <div class="ecl-row">
+        <div class="ecl-col-sm ecl-footers__column">
+          <?php print render($footer_left); ?>
+        </div>
+        <div class="ecl-col-sm ecl-footers__column">
+           <?php print render($footer_middle); ?>
+        </div>
+        <div class="ecl-col-sm ecl-footers__column">
+          <?php print render($footer_right); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ecl-footers__ec">
+    <div class="ecl-container">
+      <div class="ecl-row">
+        <div class="ecl-col-sm">
+          <?php print render($footer_bottom); ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
