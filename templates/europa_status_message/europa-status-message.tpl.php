1313956d6495d8c2ff35910b472cc9e45d12160c
Pol Dellaiera <d.paolino@gmail.com>
2017-06-30 21:16:45 +0200

    NEPT-1195: Quick typo fix in the Europa status message template. (#194)

--- templates/europa_status_message/europa-status-message.tpl.php
+++ templates/europa_status_message/europa-status-message.tpl.php
@@ -7,8 +7,8 @@
 ?>
 
 <div class="ecl-messages <?php print $message_classes; ?>">
-  <span class="ecl-sr-only"><?php print $message_type; ?> </span>
-  <button type="button" class="ecl-message__dismiss" aria-label="Dismiss this alert">&times;</a>
+  <span class="ecl-sr-only"><?php print $message_type; ?></span>
+  <button type="button" class="ecl-message__dismiss" aria-label="Dismiss this alert">&times;</button>
 
   <?php if ($message_title): ?>
       <div class="ecl-message__title"><?php print $message_title; ?></div>
@@ -21,5 +21,4 @@
       <?php print render($message_body); ?>
     </p>
   <?php endif; ?>
-
 </div>
