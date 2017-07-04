6cf5e9044a07abdfd118150e9556e7713f224004
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-07-04 14:42:22 +0200

    NEPT-1068: Update footer classes that were changed.

--- templates/footer/footer.tpl.php
+++ templates/footer/footer.tpl.php
@@ -8,7 +8,7 @@
 
 <footer class="ecl-footer">
   <?php if (!empty($footer_improved)): ?>
-    <div class="ecl-footers__site-identity">
+    <div class="ecl-footer__site-identity">
       <div class="ecl-container">
         <div class="ecl-row">
           <?php print render($footer_improved); ?>
@@ -16,23 +16,23 @@
       </div>
     </div>
   <?php endif; ?>
-  <div class="ecl-footers__site-corporate">
+  <div class="ecl-footer__site-corporate">
     <div class="ecl-container">
       <div class="ecl-row">
-        <div class="ecl-col-sm ecl-footers__column">
+        <div class="ecl-col-sm ecl-footer__column">
           <?php print render($footer_left); ?>
         </div>
-        <div class="ecl-col-sm ecl-footers__column">
+        <div class="ecl-col-sm ecl-footer__column">
            <?php print render($footer_middle); ?>
         </div>
-        <div class="ecl-col-sm ecl-footers__column">
+        <div class="ecl-col-sm ecl-footer__column">
           <?php print render($footer_right); ?>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="ecl-footers__ec">
+  <div class="ecl-footer__ec">
     <div class="ecl-container">
       <div class="ecl-row">
         <div class="ecl-col-sm">
