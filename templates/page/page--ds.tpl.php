dce5ca40b4b24d8e1e333428c99d0480f81f45c5
imanoleguskiza <ier@opendeusto.es>
2017-07-12 13:37:43 +0200

    NEPT-1091: Implemented default site header and logo. (#203)
    * NEPT-1091: Implemented default site header and logo.
    * NEPT-1091: Removed classes and simplified logo template.
    * NEPT-1091: Removed external_class variable.
    * NEPT-1091: Change the logic for the href.
    * NEPT-1091: Made info site home page the default url.

--- templates/page/page--ds.tpl.php
+++ templates/page/page--ds.tpl.php
@@ -91,34 +91,21 @@
 </section>
 <?php endif; ?>
 
-<header class="site-header" role="banner">
-  <div class="container-fluid">
+<?php print render($site_header); ?>
 
-  <?php if (!($is_front) || theme_get_setting('ec_europa_site_header_home', 'europa')): ?>
-    <a href="<?php print $front_page; ?>" class="<?php print $logo_classes; ?>" title="<?php print $page_logo_title; ?>">
-      <span class="sr-only"><?php print $page_logo_title; ?></span>
-    </a>
-  <?php endif; ?>
-    
-  <?php if ($is_front && !empty($site_slogan)): ?>
-    <p class="site-slogan"><?php print $site_slogan; ?></p>
-  <?php endif; ?>
-
-  <?php if (!empty($page['header'])): ?>
+<?php if (!empty($page['header'])): ?>
     <section class="top-bar" aria-label="Site tools">
-      <div>
-        <div class="top-bar__wrapper">
-        <?php if ($is_front): ?>
-          <h1 class="sr-only"><?php print $site_name; ?></h1>
-          <h2 class="sr-only"><?php print t('Classes'); ?></h2>
-        <?php endif; ?>
-          <?php print render($page['header']); ?>
+        <div>
+            <div class="top-bar__wrapper">
+              <?php if ($is_front): ?>
+                  <h1 class="sr-only"><?php print $site_name; ?></h1>
+                  <h2 class="sr-only"><?php print t('Classes'); ?></h2>
+              <?php endif; ?>
+              <?php print render($page['header']); ?>
+            </div>
         </div>
-      </div>
     </section>
-  <?php endif; ?>
-  </div>
-</header>
+<?php endif; ?>
 
 <?php if (!empty($page['header_bottom'])): ?>
   <nav class="page-navigation" role="navigation">
--- templates/site_header/site-header.tpl.php
+++ templates/site_header/site-header.tpl.php
@@ -5,16 +5,14 @@
  * Contains component file.
  */
 ?>
-<header class="ecl-site-header ecl-site-header--homepage" role="banner">
- <div class="container-fluid ecl-site-header__container">
- <section class="ecl-site-header__top-bar" aria-label="Site tools">
-   <div class="ecl-site-header__top-bar__wrapper">
-     <?php print render($lang_select_site); ?>
-   </div>
- </section>
+<header class="ecl-site-header" role="banner">
+  <div class="container-fluid ecl-site-header__container">
     <?php print render($logo); ?>
-    <?php print render($site_slogan); ?>
-    <span class="ecl-sr-only"><?php print $site_name; ?></span>
-    <?php print render($search_bar); ?>
+    <section class="ecl-site-header__top-bar" aria-label="Site tools">
+      <div class="ecl-site-header__top-bar__wrapper">
+        <?php print render($lang_select_site); ?>
+        <?php print render($search_bar); ?>
+      </div>
+    </section>
   </div>
 </header>
