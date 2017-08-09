72b35f8fa4a5e566c1965394fbfe29dbf2ba9517
Gilles Deudon <gilles.deudon@atos.net>
2017-08-09 14:50:53 +0200

    NEPT-1132: Fix search header form and clean the code compare to the status of NEPT-1090.

---
+++ templates/form_element/form-element--textfield--nexteuropa-europa-search-search-form--QueryText.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+
+<label class="ecl-search-form__textfield-wrapper">
+  <?php print render($label); ?>
+  <?php print $element['#children']; ?>
+</label>
---
+++ templates/form_element_label/form-element-label--form-element--textfield--nexteuropa-europa-search-search-form--QueryText.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<span class="ecl-sr-only"><?php print render($form_element_label['title']); ?></span>
