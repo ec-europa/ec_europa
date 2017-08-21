dd1b018cd95f95c95502f30e44621652b769d330
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-08-21 15:38:31 +0200

    NEPT-1064: Changes in flavor of improving the compatibility with NextEuropa Formatters.

--- templates/expandable/expandable.tpl.php
+++ templates/expandable/expandable.tpl.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<div>
-  <?php print render($link); ?>
+<div<?php print $attributes; ?>>
+  <?php print render($title); ?>
   <div<?php print $content_attributes; ?>>
     <?php print render($body); ?>
   </div>
