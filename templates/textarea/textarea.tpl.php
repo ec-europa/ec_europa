08064af47f66cd2769761e3ebf4289aa054d410c
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-09-01 15:41:50 +0200

    NEPT-1134: Remove check_plain() from template and use the preprocess instead.

--- templates/textarea/textarea.tpl.php
+++ templates/textarea/textarea.tpl.php
@@ -5,4 +5,4 @@
  * Contains template file.
  */
 ?>
-<textarea<?php print $attributes; ?>><?php print check_plain(render($element['#value'])); ?></textarea>
+<textarea<?php print $attributes; ?>><?php print render($element['#value']); ?></textarea>
