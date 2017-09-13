30056a3f3d56153cf925c28743b555bfbffbbe61
Yemoko <mbk@kuenza.com>
2017-09-13 20:01:58 +0200

    NEPT-1076: Update field component with the community access label.

---
+++ templates/field/field--list-boolean--node--full--community.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<span <?php print $attributes; ?>>
+  <?php if(isset($label_value)): ?>
+    <?php print $label_value; ?>
+  <?php endif; ?>
+</span>
