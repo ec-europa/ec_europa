21418d733a3efda7b4fdc7fec70c942346aac97a
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-09-29 11:26:43 +0200

    NEPT-1097: Update labels component.

---
+++ templates/labels/labels--upcoming.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<span<?php print $atomium['attributes']['wrapper']->append('class', 'ecl-label--upcoming'); ?>>
+  <?php print $content; ?>
+</span>
