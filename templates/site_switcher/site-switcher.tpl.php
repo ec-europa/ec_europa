20cb672dffe7dc963d58d30c73c66dc6db7e77e1
imanoleguskiza <ier@opendeusto.es>
2017-10-27 11:52:07 +0200

    NEPT-1145: Moved the use-menu to the site-header.

--- templates/site_switcher/site-switcher.tpl.php
+++ templates/site_switcher/site-switcher.tpl.php
@@ -6,8 +6,5 @@
  */
 ?>
 <div<?php print $atomium['attributes']['wrapper']; ?>>
-    <div class="ecl-container">
-      <?php print render($menu); ?>
-    </div>
   <?php print render($links); ?>
 </div>
