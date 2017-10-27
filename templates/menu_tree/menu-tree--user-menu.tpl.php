9f73d3eece0c515c7610a16b794bafd6d9f02e0f
Victor Da Costa <vdacosta@voidtek.com>
2017-10-27 15:40:31 +0200

    NEPT-1498: Update Header the buttons according the ECL 0.0.3. (#22)
    NEPT-1498: Update Header the buttons according the ECL 0.13.0.
    NEPT-1498: Upgrade view_views_table component.
    NEPT-1498: Add a workaround waiting the new ecl package.

--- templates/menu_tree/menu-tree--user-menu.tpl.php
+++ templates/menu_tree/menu-tree--user-menu.tpl.php
@@ -5,7 +5,8 @@
  * Contains template file.
  */
 ?>
-<nav class="ecl-navigation-list-wrapper ecl-u-f-r">
+<?php // @todo: remove the 'style="float:right"' after the update of ecl. ?>
+<nav class="ecl-navigation-list-wrapper ecl-u-f-r" style="float:right">
     <h2 class="ecl-u-sr-only">User menu</h2>
     <ul<?php print $atomium['attributes']['wrapper']; ?>><?php print render($tree); ?></ul>
 </nav>
---
+++ templates/views_view_table/views-view-table.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+<?php print render($table);?>
