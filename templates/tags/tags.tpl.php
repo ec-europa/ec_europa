59952ae8069f0995fea02927defe901a8877a912
imanoleguskiza <ier@opendeusto.es>
2017-10-30 14:43:32 +0100

    NEPT-1097: Refactored for atomium 2.0.

--- templates/tags/tags.tpl.php
+++ templates/tags/tags.tpl.php
@@ -5,9 +5,9 @@
  * Contains template file.
  */
 ?>
-<div<?php print $attributes; ?>>
+<div<?php print $atomium['attributes']['wrapper']; ?>>
   <?php if (!empty($label)): ?>
-    <span<?php print $title_attributes; ?>><?php print $label ?></span>
+    <span<?php print $atomium['attributes']['title']; ?>><?php print $label ?></span>
   <?php endif; ?>
   <?php print render($items); ?>
 </div>
