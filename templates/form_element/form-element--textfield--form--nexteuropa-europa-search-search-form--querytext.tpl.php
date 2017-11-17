cbaf6ebd67688a881b5f978617277d8103764f53
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-11-17 08:07:39 +0100

    NEPT-1617: Review and cleanup component - Update search form template.

--- templates/form_element/form-element--textfield--form--nexteuropa-europa-search-search-form--querytext.tpl.php
+++ templates/form_element/form-element--textfield--form--nexteuropa-europa-search-search-form--querytext.tpl.php
@@ -5,9 +5,7 @@
  * Contains template file.
  */
 ?>
-<div<?php print $atomium['attributes']['wrapper']; ?>>
-  <label class="ecl-search-form__textfield-wrapper">
-    <?php print render($label); ?>
-    <?php print $element['#children']; ?>
-  </label>
-</div>
+<label class="ecl-search-form__textfield-wrapper">
+  <?php print render($label); ?>
+  <?php print $element['#children']; ?>
+</label>
