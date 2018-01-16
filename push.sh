a407177a1e318dd8594f5b1b0fc3f33c37f6e303
richardcanoe <richardcanoe@gmail.com>
2018-01-16 16:36:16 +0100

    Backstop folder name fix

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b gh-pages
-  git add . backstop-data/*
+  git add . backstop_data/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
