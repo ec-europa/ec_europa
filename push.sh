d217828d626251db0a853ae8903e8e1775855468
richardcanoe <richardcanoe@gmail.com>
2018-01-17 15:53:40 +0100

    Git fetch unshallow

--- push.sh
+++ push.sh
@@ -9,12 +9,13 @@ commit_website_files() {
   echo "Travis build: $TRAVIS_BUILD_NUMBER"
   git checkout -b gh-pages-$TRAVIS_BUILD_NUMBER
   git add . backstop_data/*
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit -n --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-pages https://$GH_TOKEN@github.com/richardcanoe/visregtest.git > /dev/null 2>&1
-  git push --force --set-upstream origin-pages gh-pages-$TRAVIS_BUILD_NUMBER
+  git fetch --unshallow
+  git push --force origin-pages gh-pages-$TRAVIS_BUILD_NUMBER
 }
 
 setup_git
