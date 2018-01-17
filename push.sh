d6098acc1365094f13c527743d1d754844cfd6d2
richardcanoe <richardcanoe@gmail.com>
2018-01-17 14:49:27 +0100

    Git push force used - note to test rebase

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://$GH_TOKEN@github.com/richardcanoe/visregtest.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages
+  git push --force --set-upstream origin-pages gh-pages
 }
 
 setup_git
