dc8bdb5db79d2505511bc0e338315931e9d91675
richardcanoe <richardcanoe@gmail.com>
2018-01-17 15:20:29 +0100

    Add new branch each push

--- push.sh
+++ push.sh
@@ -6,14 +6,15 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  echo "Travis build: $TRAVIS_BUILD_NUMBER"
+  git checkout -b gh-pages-$TRAVIS_BUILD_NUMBER
   git add . backstop_data/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-pages https://$GH_TOKEN@github.com/richardcanoe/visregtest.git > /dev/null 2>&1
-  git push --force --set-upstream origin-pages gh-pages
+  git push --force --set-upstream origin-pages gh-pages-$TRAVIS_BUILD_NUMBER
 }
 
 setup_git
