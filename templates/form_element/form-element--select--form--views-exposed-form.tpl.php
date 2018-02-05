7c32111059d242f9bd28901fa7528812cf70dd0f
Enrique Lacoma <elacomap@everis.com>
2018-02-05 21:11:28 +0100

    NEPT-1430: Create tpl files for select filters

--- templates/form_element/form-element--select--form--views-exposed-form.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Contains template file.
- */
-?>
-<select<?php print $atomium['attributes']['element']->append('class', 'ecl-select')->setAttribute('id', $element['#id']); ?>><?php print form_select_options($element); ?></select>
