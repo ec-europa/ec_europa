df495ef41f464990a782ed7bdb80f5520ccbe07b
Enrique Lacoma <elacomap@everis.com>
2018-02-06 08:11:54 +0100

    NEPT-1430: Add prefix to the template

--- templates/textfield/textfield--form--views-exposed-form.tpl.php
+++ templates/textfield/textfield--form--views-exposed-form.tpl.php
@@ -5,4 +5,4 @@
  * Contains template file.
  */
 ?>
-<input<?php print $atomium['attributes']['element']->append('class', "ecl-text-input")->append('id', $element['#id']); ?>/><?php print render($suffix); ?>
+<?php print render($prefix); ?><input<?php print $atomium['attributes']['element']->append('class', "ecl-text-input")->append('id', $element['#id']); ?>/><?php print render($suffix); ?>
