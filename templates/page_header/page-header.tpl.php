f62aed8cb74dfe6c70fa5605ed857fd143c31d14
Enrique Lacoma <elacomap@everis.com>
2018-02-08 16:05:43 +0100

    NEPT-1422: Implement ecl structure on page-header.tpl.php

--- templates/page_header/page-header.tpl.php
+++ templates/page_header/page-header.tpl.php
@@ -8,26 +8,29 @@
 <div class="ecl-page-header">
   <?php print render($breadcrumb); ?>
   <div class="ecl-page-header__body">
-      <?php if (!empty($identity)): ?>
-        <div class="ecl-container">
+      <div class="ecl-container">
+        <?php if (!empty($identity)): ?>
             <div class="ecl-page-header__identity">
               <?php print render($identity); ?>
             </div>
-        </div>
-      <?php endif; ?>
-      <?php if(!$ec_europa_basic_header): ?>
-        <?php if (!empty($meta)): ?>
-          <div class="ecl-page-header__meta">
-            <?php print render($meta); ?>
-          </div>
         <?php endif; ?>
-        <?php if (!empty($introduction)): ?>
-          <div class="ecl-page-header__intro">
-            <p class="ecl-paragraph ecl-paragraph--l">
-              <?php print render($introduction); ?>
-            </p>
+        <?php if(!$ec_europa_basic_header): ?>
+          <?php if (!empty($meta)): ?>
+            <div class="ecl-page-header__meta">
+              <?php print render($meta); ?>
+            </div>
+          <?php endif; ?>
+          <div class="ecl-page-header__title">
+            <h1 class="ecl-heading ecl-heading--h1 ecl-u-color-white">Business, Economy, Euro</h1>
           </div>
+          <?php if (!empty($introduction)): ?>
+            <div class="ecl-page-header__intro">
+              <p class="ecl-paragraph ecl-paragraph--l">
+                <?php print render($introduction); ?>
+              </p>
+            </div>
+          <?php endif; ?>
         <?php endif; ?>
-      <?php endif; ?>
+      </div>
   </div>
 </div>
