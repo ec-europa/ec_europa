68d7848ee05ea9b51b5c9f10ec797db7ae925bb8
Enrique Lacoma <enriquelacoma@gmail.com>
2018-02-28 13:40:01 +0100

    Nept 1430: Implement ecl for workbench filters (#55)
    * NEPT-1430: Apply ecl templates to workbench all filters
    * NEPT-1430: Template for view exposed forms
    * NEPT-1430: Create tpl file for exposed view texfield
    * NEPT-1430: Create tpl files for select filters
    * NEPT-1430: Add prefix to the template

---
+++ templates/textfield/textfield--form--views-exposed-form.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<?php print render($prefix); ?><input<?php print $atomium['attributes']['element']->append('class', "ecl-text-input")->append('id', $element['#id']); ?>/><?php print render($suffix); ?>
--- templates/views_exposed_form/views-exposed-form.tpl.php
+++ templates/views_exposed_form/views-exposed-form.tpl.php
@@ -35,26 +35,24 @@
 <div class="views-exposed-form">
   <div class="views-exposed-widgets clearfix">
     <?php foreach ($widgets as $id => $widget): ?>
-      <div id="<?php print $widget->id; ?>-wrapper" class="filters__filter views-exposed-widget views-widget-<?php print $id; ?>">
-        <?php if (!empty($widget->label)): ?>
-          <label for="<?php print $widget->id; ?>">
-            <?php print $widget->label; ?>
-          </label>
-        <?php endif; ?>
-        <?php if (!empty($widget->operator)): ?>
-          <div class="views-operator">
-            <?php print $widget->operator; ?>
-          </div>
-        <?php endif; ?>
-        <div class="views-widget">
-          <?php print $widget->widget; ?>
+      <?php if (!empty($widget->operator)): ?>
+        <div class="views-operator">
+          <?php print $widget->operator; ?>
         </div>
-        <?php if (!empty($widget->description)): ?>
-          <div class="description">
-            <?php print $widget->description; ?>
-          </div>
-        <?php endif; ?>
+      <?php endif; ?>
+      <div class="ecl-form-group">
+      <?php if (!empty($widget->label)): ?>
+        <label<?php print $atomium['attributes']['label']->append('class', 'ecl-form-label')->setAttribute('for', $widget->id); ?>>
+          <?php print $widget->label; ?>
+        </label>
+      <?php endif; ?>
+      <?php print $widget->widget; ?>
       </div>
+      <?php if (!empty($widget->description)): ?>
+        <div class="description">
+          <?php print $widget->description; ?>
+        </div>
+      <?php endif; ?>
     <?php endforeach; ?>
     <?php if (!empty($sort_by)): ?>
       <div class="views-exposed-widget views-widget-sort-by">
@@ -65,7 +63,7 @@
       </div>
     <?php endif; ?>
     <?php if (!empty($items_per_page)): ?>
-      <div class="views-exposed-widget views-widget-per-page">
+      <div class="ecl-form-group">
         <?php print $items_per_page; ?>
       </div>
     <?php endif; ?>
