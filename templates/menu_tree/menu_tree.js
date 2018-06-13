1d1d324a52018827b4715b3b59dfd33b006c3e64
papegaill <a@bypopcorn.com>
2018-06-13 10:57:05 +0200

    Better way to solve the issue.
    https://www.drupal.org/project/webform/issues/2915611#comment-12456529

--- templates/menu_tree/menu_tree.js
+++ templates/menu_tree/menu_tree.js
@@ -6,7 +6,9 @@
 (function ($) {
   Drupal.behaviors.ecEuropaMenuTree = {
     attach: function (context) {
-      ECL.megamenu('.ecl-navigation-menu__root');
+      if (jQuery(context).find('.ecl-navigation-menu__root').once('ecEuropaMenuTree').length > 0) {
+        ECL.megamenu('.ecl-navigation-menu__root');
+      };
     }
   };
 })(jQuery);
