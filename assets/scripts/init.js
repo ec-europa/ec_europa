aab6539036a14dfeab3a9293f9b218cd0bae78fd
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-06-14 10:03:41 +0200

    NEPT-1988: Make sure that JS is only triggered once on ECL expandables and megamenu.

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,10 +1,12 @@
 
 (function ($) {
-  Drupal.behaviors.ecEuropaExpandable = {
-    attach: function (context, settings) {
-      // This must only be instantiated once.
-      // This will be probably removed once INNO team found out a workaround.
-      ECL.initExpandables('.ecl-navigation-menu__toggle');
-    }
-  };
+    Drupal.behaviors.ecEuropaExpandable = {
+        attach: function (context, settings) {
+            // This must only be instantiated once.
+            // This will be probably removed once INNO team found out a workaround.
+            $('.ecl-navigation-menu__toggle').once('ecEuropaExpandable', function() {
+                ECL.initExpandables('.ecl-navigation-menu__toggle');
+            });
+        }
+    };
 }(jQuery));
