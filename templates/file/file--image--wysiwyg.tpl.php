560344087bb73deba2716975415ed00efcc08424
gillesdeudon <gillesdeudon@users.noreply.github.com>
2018-08-27 14:59:58 +0200

    NEPT-2038: Add wysiwyg templates for file entity and file container (#75)
    * NEPT-2038: Update Travis configuration to prevent memory issues with PHP 5.6.
    
    * NEPT-2038: Add wysiwyg templates for file entity and file container.
    
    * NEPT-2038: Made custom container.tpl follow the original more closely.

---
+++ templates/file/file--image--wysiwyg.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Template to display an image file value inside a WYSIWYG field.
+ *
+ * It diverts from the file_entity one by displaying the value without any
+ * additional DIV container.
+ * It is then recommended to use it without setting image caption.
+ *
+ * It is implemented for NEPT-2038.
+ */
+?>
+
+<?php print render($content); ?>
