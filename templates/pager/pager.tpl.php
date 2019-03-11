fc1b0f88420e081cdcdea66e14e7cc03df0766b5
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-11 18:33:42 +0100

    Issue #90: Fix pager component.

--- templates/pager/pager.tpl.php
+++ templates/pager/pager.tpl.php
@@ -17,7 +17,7 @@
 
     <?php for ($cursor = $pager_first; $cursor <= $pager_last; $cursor++): ?>
 
-      <?php if (($pager_last - $quantity >= 1) && $cursor === $pager_first): ?>
+      <?php if (($pager_last - $quantity >= 1) && (int) $cursor === (int) $pager_first): ?>
         <li class="ecl-pager__item ecl-pager__item--first">
           <a class="ecl-pager__link" title="<?php print $goto_title ?> 1" href="<?php print $links['pager_first']['url'] ?>">1</a>
         </li>
@@ -26,7 +26,7 @@
         <?php endif ?>
       <?php endif ?>
 
-      <?php if ($cursor === $pager_current): ?>
+      <?php if ((int) $cursor === (int) $pager_current): ?>
         <li class="ecl-pager__item ecl-pager__item--current"><span class="ecl-pager__item-text"><?php print $page_label ?></span> <?php print $pager_current ?></li>
       <?php else: ?>
         <li class="ecl-pager__item">
@@ -34,7 +34,7 @@
         </li>
       <?php endif ?>
 
-      <?php if (($pager_first + $quantity < $pager_max) && $cursor === $pager_last): ?>
+      <?php if (($pager_first + $quantity < $pager_max) && (int) $cursor === (int) $pager_last): ?>
         <li class="ecl-pager__item ecl-pager__item--ellipsis">…</li>
         <li class="ecl-pager__item ecl-pager__item--last">
           <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $pager_max ?>" href="<?php print $links['pager_last']['url'] ?>"><?php print $pager_max ?></a>
