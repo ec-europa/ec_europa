4733766c618009e0af0ec8df5fa2d1053ea1ad67
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-14 14:05:40 +0100

    NEPT-1419: Add site slogan to the header

--- templates/site_header/site-header.tpl.php
+++ templates/site_header/site-header.tpl.php
@@ -12,6 +12,7 @@
   <?php print render($site_switcher); ?>
   <div class="ecl-container ecl-site-header__banner">
     <?php print render($logo); ?>
+    <?php print render($site_slogan); ?>
     <?php print render($lang_select_site); ?>
     <?php print render($search_bar); ?>
   </div>
