7fdfe66e6bd1882b4999933e0c2d38d834047643
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-19 15:40:38 +0100

    Issue #98: Update ECL Builder configuration.

--- ecl-builder.config.js
+++ ecl-builder.config.js
@@ -1,48 +1,57 @@
-const path = require("path");
-const source_folder = __dirname + "/node_modules/@ec-europa";
+const path = require('path');
+const pkg = require('./package.json');
+
+const isProd = process.env.NODE_ENV === 'production';
+const outputFolder = __dirname + '/assets';
+
+const nodeModules = __dirname + '/node_modules';
+
+// SCSS includePaths
+const includePaths = [nodeModules];
+
+const banner = `${pkg.name} - ${
+  pkg.version
+} - Based on ECL ${pkg.dependencies['@ecl/ec-preset-website']} - Built on ${new Date().toISOString()}`;
 
 module.exports = {
   scripts: [
     {
-      entry: path.resolve(__dirname, "assets/scripts/entry.js"),
-      dest: path.resolve(__dirname, "assets/scripts/europa.js"),
+      entry: path.resolve(nodeModules, '@ecl/ec-preset-website/ec-preset-website.js'),
+      dest: path.resolve(outputFolder, 'scripts/base.js'),
       options: {
-        sourceMap: false,
-        moduleName: "ECL"
+        banner,
+        moduleName: 'ECL',
+        sourceMap: isProd ? false : 'inline'
       }
     }
   ],
   styles: [
     {
-      entry: path.resolve(
-        source_folder,
-        "ecl-preset-full/index.scss"
-      ),
-      dest: path.resolve(__dirname, "assets/styles/europa.css"),
+      entry: path.resolve(__dirname, 'sass/style.scss'),
+      dest: path.resolve(outputFolder, 'styles/europa.css'),
       options: {
-        sourceMap: false
-      }
-    },
-    {
-      entry: path.resolve(__dirname, "assets/src/scss/editor-index.scss"),
-      dest: path.resolve(__dirname, "assets/styles/wysiwyg/editor.css"),
-      options: {
-        sourceMap: false
+        banner,
+        includePaths,
+        sourceMap: isProd ? 'file' : true
       }
     }
   ],
   copy: [
-    {
-      from: path.resolve(source_folder, "ecl-icons/fonts"),
-      to: path.resolve(__dirname, "assets/fonts")
-    },
-    {
-      from: path.resolve(source_folder, "ecl-logos/images"),
-      to: path.resolve(__dirname, "assets/images")
-    },
-    {
-      from: path.resolve(source_folder, "ecl-social-icons/images"),
-      to: path.resolve(__dirname, "assets/images")
-    }
+    { from: path.resolve(nodeModules, '@ecl/generic-style-icon/fonts'), to: path.resolve(outputFolder, 'fonts') },
+    { from: path.resolve(nodeModules, '@ecl/generic-component-form-checkbox/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-form-feedback-message/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-form-radio/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-form-select/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-logo/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-message/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/generic-component-social-icon/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-form-checkbox/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-form-checkbox/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-form-feedback-message/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-form-radio/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-form-select/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-logo/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-message/images'), to: path.resolve(outputFolder, 'images')},
+    { from: path.resolve(nodeModules, '@ecl/ec-component-social-icon/images'), to: path.resolve(outputFolder, 'images')}
   ]
 };
