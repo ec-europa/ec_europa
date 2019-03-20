949186f32e6096533951a432905b7d4e12191d41
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-20 09:53:27 +0100

    Issue #97: Fix status messages component.

--- templates/status_messages/status-messages.tpl.php
+++ templates/status_messages/status-messages.tpl.php
@@ -8,7 +8,7 @@
 <?php foreach ($status_messages['status_messages'] as $key => $status_message): ?>
   <div<?php print $atomium['attributes']['status-' . $key]; ?>>
     <span class="ecl-u-sr-only"><?php print render($status_message['status_heading']); ?></span>
-    <button type="button" class="ecl-message__dismiss" aria-label="Dismiss this alert">&times;</button>
+    <button type="button" class="ecl-message__dismiss" aria-label="Close this message">Close</button>
     <?php print render($status_message['messages']); ?>
   </div>
 <?php endforeach; ?>
