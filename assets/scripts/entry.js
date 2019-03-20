67ccdc46cb001c559375a00a67353442c087f226
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-20 15:06:32 +0100

    Issue #97: Remove generated assets from the repository.

--- assets/scripts/entry.js
+++
@@ -1,8 +0,0 @@
-// Taking modules from ECL.
-export * from "../../node_modules/@ec-europa/ecl-accordions/accordions";
-export * from "../../node_modules/@ec-europa/ecl-dropdowns/ecl-dropdowns";
-export * from "../../node_modules/@ec-europa/ecl-expandables/expandables";
-export * from "../../node_modules/@ec-europa/ecl-messages/messages";
-export * from "../../node_modules/@ec-europa/ecl-navigation-menus/megamenu";
-export * from "../../node_modules/@ec-europa/ecl-tables/ecl-tables";
-export * from "../../node_modules/@ec-europa/ecl-tabs/tabs";
