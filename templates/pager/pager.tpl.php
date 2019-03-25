44169b72b92d3d34a536781d4f8952c4bf99fc41
AritoMelo <aritomelo@gmail.com>
2019-03-25 18:44:14 +0100

    Issue #97: Updating pager markup to reflect ECL 1.4.0.

--- templates/pager/pager.tpl.php
+++ templates/pager/pager.tpl.php
@@ -19,7 +19,9 @@
 
       <?php if (($pager_last - $quantity >= 1) && (int) $cursor === (int) $pager_first): ?>
         <li class="ecl-pager__item ecl-pager__item--first">
-          <a class="ecl-pager__link" title="<?php print $goto_title ?> 1" href="<?php print $links['pager_first']['url'] ?>">1</a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> 1" href="<?php print $links['pager_first']['url'] ?>">
+            <span class="ecl-u-sr-only">Page </span>1
+          </a>
         </li>
         <?php if ($pager_last - $quantity > 1): ?>
           <li class="ecl-pager__item ecl-pager__item--ellipsis">…</li>
@@ -27,17 +29,23 @@
       <?php endif ?>
 
       <?php if ((int) $cursor === (int) $pager_current): ?>
-        <li class="ecl-pager__item ecl-pager__item--current"><span class="ecl-pager__item-text"><?php print $page_label ?></span> <?php print $pager_current ?></li>
+        <li class="ecl-pager__item ecl-pager__item--current" aria-current="true">
+          <span class="ecl-pager__item-text"><?php print $page_label ?></span><?php print $pager_current ?>
+        </li>
       <?php else: ?>
         <li class="ecl-pager__item">
-          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $links['pager_link__' . $cursor]['label'] ?>" href="<?php print $links['pager_link__' . $cursor]['url'] ?>"><?php print $links['pager_link__' . $cursor]['label'] ?></a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $links['pager_link__' . $cursor]['label'] ?>" href="<?php print $links['pager_link__' . $cursor]['url'] ?>">
+            <?php print $links['pager_link__' . $cursor]['label'] ?>
+          </a>
         </li>
       <?php endif ?>
 
       <?php if (($pager_first + $quantity < $pager_max) && (int) $cursor === (int) $pager_last): ?>
         <li class="ecl-pager__item ecl-pager__item--ellipsis">…</li>
         <li class="ecl-pager__item ecl-pager__item--last">
-          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $pager_max ?>" href="<?php print $links['pager_last']['url'] ?>"><?php print $pager_max ?></a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $pager_max ?>" href="<?php print $links['pager_last']['url'] ?>">
+            <span class="ecl-u-sr-only"><?php print $page_label ?></span><?php print $pager_max ?>
+          </a>
         </li>
       <?php endif ?>
 
