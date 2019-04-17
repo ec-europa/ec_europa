ac8f355abfc98cf948fb44393790749b2ec0ea06
AritoMelo <aritomelo@gmail.com>
2019-04-17 11:48:11 +0200

    Issue #128: Field component update.

--- templates/field/field.tpl.php
+++ templates/field/field.tpl.php
@@ -5,12 +5,4 @@
  * Contains template file.
  */
 ?>
-
-<div<?php print $atomium['attributes']['wrapper']; ?>>
-    <div class="ecl-field__label"><?php print $label ?></div>
-    <div class="ecl-field__body">
-      <?php foreach ($items as $delta => $item) : ?>
-          <div<?php print $atomium['attributes'][$delta]; ?>><?php print render($item); ?></div>
-      <?php endforeach; ?>
-    </div>
-</div>
+<?php print render($field); ?>
