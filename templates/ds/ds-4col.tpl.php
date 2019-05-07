63149685483c2f768b67ba76425fa1b02381e809
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-05-07 16:10:47 +0200

    Issue #137: Use Taskman (#138)
    * Issue #137: Use Taskman.
    
    * Issue #137: Fix typo in documentation.
    
    * Issue #137: Update Drone configuration.
    
    * Issue #114: Update composer.json.
    
    * Issue #137: Fix CS.
    
    * Issue #137: Update composer.json.
    
    * Issue #137: Update composer.json.
    
    * Issue #137: Update composer.json.
    
    (cherry picked from commit 1ff2c4bd078a1e3b6cbc0dcceb6ba1eca5092a4d)

--- templates/ds/ds-4col.tpl.php
+++ templates/ds/ds-4col.tpl.php
@@ -5,13 +5,13 @@
  * Display Suite 4 column template.
  */
 ?>
-<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-4col <?php print $classes;?>">
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-4col <?php print $classes; ?>">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
-  <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ecl-row <?php print $classes;?>">
+  <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ecl-row <?php print $classes; ?>">
 
     <<?php print $first_wrapper ?> class="ecl-col<?php print $first_classes; ?>">
       <?php print $first; ?>
