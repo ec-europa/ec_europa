85ec406710f260f8fc7b014e6ca9e67740d5a1ee
AritoMelo <aritomelo@gmail.com>
2019-06-20 11:47:26 +0200

    Issue #159: AutoFix.

--- templates/ds/ds-2col-stacked-fluid.tpl.php
+++ templates/ds/ds-2col-stacked-fluid.tpl.php
@@ -13,25 +13,25 @@ if (($left && !$right) || ($right && !$left)) {
 ?>
 <<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-2col-stacked-fluid <?php print $classes; ?> clearfix">
 
-  <?php if (isset($title_suffix['contextual_links'])): ?>
-  <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
+    <?php if (isset($title_suffix['contextual_links'])) : ?>
+        <?php print render($title_suffix['contextual_links']); ?>
+    <?php endif; ?>
 
   <<?php print $header_wrapper ?> class="group-header<?php print $header_classes; ?>">
     <?php print $header; ?>
   </<?php print $header_wrapper ?>>
 
-  <?php if ($left): ?>
+    <?php if ($left) : ?>
     <<?php print $left_wrapper ?> class="group-left<?php print $left_classes; ?>">
-      <?php print $left; ?>
+        <?php print $left; ?>
     </<?php print $left_wrapper ?>>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ($right): ?>
+    <?php if ($right) : ?>
     <<?php print $right_wrapper ?> class="group-right<?php print $right_classes; ?>">
-      <?php print $right; ?>
+        <?php print $right; ?>
     </<?php print $right_wrapper ?>>
-  <?php endif; ?>
+    <?php endif; ?>
 
   <<?php print $footer_wrapper ?> class="group-footer<?php print $footer_classes; ?>">
     <?php print $footer; ?>
@@ -39,6 +39,6 @@ if (($left && !$right) || ($right && !$left)) {
 
 </<?php print $layout_wrapper ?>>
 
-<?php if (!empty($drupal_render_children)): ?>
-  <?php print $drupal_render_children ?>
+<?php if (!empty($drupal_render_children)) : ?>
+    <?php print $drupal_render_children ?>
 <?php endif; ?>
