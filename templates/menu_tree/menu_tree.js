72b2c800e4ce42556cc38ee7af5c257328b245be
drishu <aszilagyi@live.com>
2020-01-29 15:10:26 +0200

    NEPT-2781: Fix Main Menu Navigation collapse issue.

--- templates/menu_tree/menu_tree.js
+++ templates/menu_tree/menu_tree.js
@@ -9,6 +9,11 @@
             $('.ecl-navigation-menu__root').once('ecEuropaMenuTree', function() {
                 ECL.megamenu('.ecl-navigation-menu__root');
             });
+            if ($('.ecl-navigation-menu__group').length) {
+              $('.ecl-navigation-menu__group').once('ecEuropaSubmenuTree', function() {
+                  ECL.megamenu('.ecl-navigation-menu__group');
+              });
+            }
         }
     };
     Drupal.behaviors.megaMenu = {
